8564fc93ef1d613f892b4e2c592bb12c315a02ce
Shane Steszyn <shane@steszyn.ca>
2016-01-16 23:30:26 -0500

    Some cool self building stuff

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-
+var http = require('http');
 var express = require('express');
 var app = express();
 
-app.use('/static', express.static('public'));
-
-app.get('/', function (req, res) {
-  res.send('Website');
+process.argv.forEach(function(v){
+  if(v === '--development') {
+    global.PRODUCTION = false;
+    global.CONFIG = require('./config/main.js').development;
+  } else {
+    global.PRODUCTION = true;
+    global.CONFIG = require('./config/main.js').production;
+  }
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
-})
+app.use('/static', express.static('public'));
+
+app.use(express.static('app/public'));
+
+app.use('/', function(req, res, next){
+  console.log('/');
+  res.sendfile('public/index.html')
+});
+
+
+console.log('Starting a server on '+CONFIG.port);
+
+http
+  .createServer(app)
+  .listen(CONFIG.port);
